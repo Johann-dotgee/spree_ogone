154cab944c2045441b3171b4c724b0212e07424b
Johann <johann@dotgee.fr>
2012-08-03 16:46:30 +0200

    preferences

--- app/models/spree/gateway/ogone.rb
+++ app/models/spree/gateway/ogone.rb
@@ -1,5 +1,9 @@
 module Spree
   class Gateway::Ogone < Gateway
+  	preference :login, :string
+    preference :password, :string
+
+    attr_accessible :preferred_login, :preferred_password
 
     def provider_class
       ActiveMerchant::Billing::OgoneGateway
